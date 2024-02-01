5421945bef91f5db08c28f4c7eba26c1f6771a0e
Meital <meiti2002@hotmail.com>
2024-02-01 20:55:04 -0300

    commit firebase

---
+++ vite-project/src/app/Views/Show/Show.jsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import {collection,getDocs, deleteDoc,doc} from "firebase/firestore"
+import { db } from "../../firebase/firebase.js"
+
+import Swal from "sweetalert2"
+import withReactContent from "sweetalert2-react-content"
+const mySwal = withReactContent(Swal)
+
+export const Show = () =>{
+const [deportes, serDeportes] =useState([])
+
+const deportesCollection = collection(db, "Deportes")
+
+const getDeportes = async () =>{
+    const data = await getDocs(deportesCollection)
+}
+
+    return (
+        <h1>Datos</h1>
+    )
+} 
